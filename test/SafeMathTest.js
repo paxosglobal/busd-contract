d024ed1e127c30fb2a0d4faf6d0dd3c79dea11a6
Andres Aiello <aaiello@gmail.com>
2021-02-02 10:58:51 -0300

    BUSD-Update-dependencies

--- test/SafeMathTest.js
+++ test/SafeMathTest.js
@@ -1,43 +1,43 @@
 // It's hard to get full test coverage on SafeMath testing just the Stablecoin contract.
 // This is the openzeppelin-solidity test for add and sub as of the time of writing
 
-const {BN, constants, shouldFail} = require('openzeppelin-test-helpers');
+const { BN, constants, shouldFail } = require("@openzeppelin/test-helpers");
 const { MAX_UINT256 } = constants;
 
-const SafeMathMock = artifacts.require('SafeMathMock');
+const SafeMathMock = artifacts.require("SafeMathMock");
 
-contract('SafeMath', function () {
-  beforeEach(async function () {
+contract("SafeMath", function() {
+  beforeEach(async function() {
     this.safeMath = await SafeMathMock.new();
   });
 
-  describe('add', function () {
-    it('adds correctly', async function () {
-      const a = new BN('5678');
-      const b = new BN('1234');
+  describe("add", function() {
+    it("adds correctly", async function() {
+      const a = new BN("5678");
+      const b = new BN("1234");
 
       (await this.safeMath.add(a, b)).should.be.bignumber.equal(a.add(b));
     });
 
-    it('reverts on addition overflow', async function () {
+    it("reverts on addition overflow", async function() {
       const a = MAX_UINT256;
-      const b = new BN('1');
+      const b = new BN("1");
 
       await shouldFail.reverting(this.safeMath.add(a, b));
     });
   });
 
-  describe('sub', function () {
-    it('subtracts correctly', async function () {
-      const a = new BN('5678');
-      const b = new BN('1234');
+  describe("sub", function() {
+    it("subtracts correctly", async function() {
+      const a = new BN("5678");
+      const b = new BN("1234");
 
       (await this.safeMath.sub(a, b)).should.be.bignumber.equal(a.sub(b));
     });
 
-    it('reverts if subtraction result would be negative', async function () {
-      const a = new BN('1234');
-      const b = new BN('5678');
+    it("reverts if subtraction result would be negative", async function() {
+      const a = new BN("1234");
+      const b = new BN("5678");
 
       await shouldFail.reverting(this.safeMath.sub(a, b));
     });
